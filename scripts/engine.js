5c6b5c7b7645a867dbb4d79bc1215982dd9d8402
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-05-25 11:48:25 +0100

    start of fixed time step: this function only redraws the screen at fixed intervals to accommodate slow devices

--- scripts/engine.js
+++ scripts/engine.js
@@ -4,14 +4,30 @@ const engine = {
   update: undefined,
   render: undefined,
 
+  accumulated_time: 0,
+  current_time: 0,
+  time_step: 1000 / 30,
+
   cycle(time_stamp) {
     window.requestAnimationFrame(this.startCycle);
-    this.update();
+
+    this.elapsed_time = time_stamp - this.current_time;
+    this.accumulated_time += this.elapsed_time;
+    this.current_time = time_stamp;
+
+    while (this.accumulated_time >= this.time_step) {
+      this.update();
+
+      this.accumulated_time -= this.time_step;
+    }
+
+    this.accumulated_time;
+
     this.render();
   },
 
   start() {
-    //   'this' refers to 'window' unless this is bound to 'enegine'
+    //   'this' refers to 'window' unless 'this' is bound to 'enegine'
     window.requestAnimationFrame(this.startCycle);
   },
 
